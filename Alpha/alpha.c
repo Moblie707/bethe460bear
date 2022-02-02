2f812f08cdc0d02b88114794a871c27dde966743
Will Baglivio <william.baglivio@yahoo.com>
2022-02-02 12:52:59 -0500

    Ready to grade!

--- Alpha/alpha.c
+++ Alpha/alpha.c
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 
+int linsearch(int arr[], int n, int x);
+
 int main(int argc, char * argv[])
 {
 	// Make sure user entered correct number of arguments
@@ -27,29 +29,47 @@ int main(int argc, char * argv[])
 			return 0;
 		}
 
+		// Arrays of alphabetic labels
+		char labels[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+		int ids[26];
+
 		// Parent Id
 		int pid = getpid();
+		ids[0] = pid;
 
 		// Create N-1 processes
 		int i;
-		for (i = 0; i < (N-1); i++)
+		for (i = 1; i < N; i++)
 		{
 			// Only create process if parent
 			if (pid == getpid())
 			{
 				fork();
 			}
+
+			// Assign label to id
+			if (pid != getpid())
+			{
+				ids[i] = getpid();
+			}
 		}
+
+		// Perform linear search to find our label
+		int id = getpid();
+		int indx = linsearch(ids,N,id);
+		char label = labels[indx];
 
 		// Print process N times
 		int j,k,m,x;
 		for (i = 0; i < N; i++)
 		{
-			printf(":%d\n",getpid());
+			// Print info
+			printf("%c:%d\n",label,id);
 
+			// Busy code
 			for (j = 0; j < 1000; j++)
 			{
-				for (k = 0; k < 1000; k++)
+				for (k = 0; k < 100; k++)
 				{
 					for (m = 0; m < 100; m++)
 					{
@@ -62,3 +82,33 @@ int main(int argc, char * argv[])
 
 	return 0;
 }
+
+int linsearch(int arr[], int n, int x)
+{
+	// Basic linear search to find given element, x,
+	// in array of length n.
+	//
+	// Returns the index it was found at.
+
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (arr[i] == x)
+			break;
+	}
+
+	return i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
